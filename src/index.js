8eafc11bb0f77af16019f9e7570051d7bbee0338
Roman Shalupenko <itiszella@gmail.com>
2020-09-30 16:49:12 +0300

    feat: complete task

--- src/index.js
+++ src/index.js
@@ -1,12 +1,30 @@
+exports.min = function min(array) {
+    if (!array || array.length === 0) {
+        return 0;
+    } else {
+        return array.reduce(function (a, b) {
+            return Math.min(a, b);
+        }, 0);
+    }
+};
 
-exports.min = function min (array) {
-  return 0;
-}
+exports.max = function max(array) {
+    if (!array || array.length === 0) {
+        return 0;
+    } else {
+        return array.reduce(function (a, b) {
+            return Math.max(a, b);
+        }, 0);
+    }
+};
 
-exports.max = function max (array) {
-  return 0;
-}
-
-exports.avg = function avg (array) {
-  return 0;
-}
+exports.avg = function avg(array) {
+    if (!array || array.length === 0) {
+        return 0;
+    } else {
+        let sum = array.reduce(function (a, b) {
+            return a + b;
+        });
+        return sum / array.length;
+    }
+};
